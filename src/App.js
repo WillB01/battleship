9c7bdae463a37561eb8dbdada1f3f04b64d184d6
William <william.hello@outlook.com>
2020-11-02 14:09:20 +0100

    Remove console.log

--- src/App.js
+++ src/App.js
@@ -19,17 +19,6 @@ const App = () => {
   const [state, dispatch] = useReducer(gameReducer, initialState);
   const [roomState, roomDispatch] = useReducer(roomReducer, roomInitialState);
 
-  useEffect(() => {
-    console.log('init app');
-    console.log(process.env.REACT_APP_API_KEY);
-    console.log(process.env.REACT_APP_URL);
-
-    // getAllRooms(data => {
-    //   console.log(data);
-    //   // dispatch({ type: 'SET-ALL-ROOM', acion: data });
-    // });
-  }, []);
-
   return (
     <div className="App">
       <GameContext.Provider value={{ state, dispatch }}>
